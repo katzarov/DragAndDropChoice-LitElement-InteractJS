3d74052c553cdf40d313261b74b593da9b0ae083
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2021-02-11 14:39:46 +0200

    change bundler entry

--- rollup.config.js
+++ rollup.config.js
@@ -18,9 +18,9 @@ import resolve from 'rollup-plugin-node-resolve';
 import replace from '@rollup/plugin-replace';
 
 export default {
-  input: 'my-element.js',
+  input: 'index.js',
   output: {
-    file: 'my-element.bundled.js',
+    file: 'index.bundled.js',
     format: 'esm',
   },
   onwarn(warning) {
