b443937766cb1d140587a4b7f4453b238a3621dc
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2021-02-18 18:57:31 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,2 @@
-import "./components/count-action";
-import "./components/count-progres";
-import "./components/main-app";
 import "./components/drag-choice-element";
 import "./components/drag-choice-wrapper";
