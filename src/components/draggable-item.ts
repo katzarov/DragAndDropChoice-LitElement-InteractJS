8ac24f88adc31007b57746ec970805be6da7aec5
Lyoubomir Katzarov <lyoubomir.katzarov@yahoo.com>
2021-02-18 18:55:16 +0200

    pass attribute

--- src/components/draggable-item.ts
+++ src/components/draggable-item.ts
@@ -7,7 +7,7 @@ export class DraggableItem extends LitElement {
     static get styles() {
         return css`
           div { 
-                width: 25%;
+                width: 100px;
                 min-height: 6.5em;
                 margin: 1rem 0 0 1rem;
                 background-color: #29e;
@@ -17,13 +17,19 @@ export class DraggableItem extends LitElement {
                 touch-action: none;
                 user-select: none;
                 -webkit-transform: translate(0px, 0px);
-                transform: translate(0px, 0px);
+                transform: translate(0px, 0px);  
+                display: inline-block;
            }
         `;
     }
 
+    connectedCallback() {
+        super.connectedCallback()
+        console.log(this.id)
+    }
+
     render() {
-        return html`<div class="draggable">
+        return html`<div class="draggable drag-drop">
             <p>
                 <slot></slot>
             </p>
--- src/components/main-app.ts
+++ src/components/main-app.ts
@@ -2,34 +2,122 @@
 import "./draggable-item";
 import { LitElement, html, customElement } from 'lit-element';
 
-import * as interact from 'interactjs'
-// import {interactjs} from '../node_modules/interactjs/index'
-
-// import * as interact from 'interactjs/dist/interact.js';
-
-
+import interact from 'interactjs'
 
 import { data } from '../data';
 
 @customElement('main-app')
 export class MainApp extends LitElement {
 
-
     connectedCallback() {
         super.connectedCallback()
-        
-        console.log(interact)
-        
-    }
 
+        console.log(interact('.draggable'))
+        console.log(interact('.draggable').draggable())
+
+
+
+        interact('.draggable')
+            .draggable({
+                inertia: true,
+                autoScroll: true,
+                listeners: {
+                    // call this function on every dragmove event
+                    move: dragMoveListener,
+
+                    // call this function on every dragend event
+                    end(event) {
+                        var textEl = event.target.querySelector('p')
+
+                        textEl && (textEl.textContent =
+                            'moved a distance of ' +
+                            (Math.sqrt(Math.pow(event.pageX - event.x0, 2) +
+                                Math.pow(event.pageY - event.y0, 2) | 0))
+                                .toFixed(2) + 'px')
+                    }
+                }
+            })
+
+        function dragMoveListener(event) {
+            var target = event.target
+            // keep the dragged position in the data-x/data-y attributes
+            var x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx
+            var y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy
+
+            // translate the element
+            target.style.webkitTransform =
+                target.style.transform =
+                'translate(' + x + 'px, ' + y + 'px)'
+
+            // update the posiion attributes
+            target.setAttribute('data-x', x)
+            target.setAttribute('data-y', y)
+        }
+
+        interact('.dropzone').dropzone({
+            overlap: 0.75,
+            ondropactivate: function (event) {
+                // add active dropzone feedback
+                event.target.classList.add('drop-active')
+            },
+            ondragenter: function (event) {
+                var draggableElement = event.relatedTarget
+                var dropzoneElement = event.target
+
+                // feedback the possibility of a drop
+                dropzoneElement.classList.add('drop-target')
+                draggableElement.classList.add('can-drop')
+                draggableElement.textContent = 'Dragged in'
+            },
+            ondragleave: function (event) {
+                // remove the drop feedback style
+                event.target.classList.remove('drop-target')
+                event.relatedTarget.classList.remove('can-drop')
+                event.relatedTarget.textContent = 'Dragged out'
+            },
+            ondrop: function (event) {
+                event.relatedTarget.textContent = 'Dropped'
+            },
+            ondropdeactivate: function (event) {
+                // remove active dropzone feedback
+                event.target.classList.remove('drop-active')
+                event.target.classList.remove('drop-target')
+            }
+        })
+
+        interact('.drag-drop')
+            .draggable({
+                inertia: true,
+                modifiers: [
+                    interact.modifiers.restrictRect({
+                        restriction: 'parent',
+                        endOnly: true
+                    })
+                ],
+                autoScroll: true,
+                // dragMoveListener from the dragging demo above
+                listeners: { move: dragMoveListener }
+            })
+
+    }
 
     render() {
         return html`
         ${data.map(choice => html`
-            <draggable-item>
+            <draggable-item .id=${choice.id}>
                 ${choice.value}
             </draggable-item>
+
         `)}
+        
+        <div class="draggable drag-drop">
+            <p>
+                CONCRETE
+            </p>
+        </div>
+        <div class="dropzone" style="width: 600px; height: 200px; border: 1px solid black">shadow dropzone</div> 
+
+
     `;
     }
 }
